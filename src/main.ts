d8821c35c4671dbfa7348757f9d397a671199502
mustafaprogramming <mustafaghaffar763@gmail.com>
2024-06-19 00:41:53 +0500

    Adding Quiz

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-#! ./usr/bin/env node
+#! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
 console.log(chalk.greenBright(`\n\n\t   _-^+-^+      ------     === o o 0 o o ===     ------      +^-+^-_`));
